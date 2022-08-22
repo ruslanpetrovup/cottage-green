53cdceae3370f46815d5cec642f053f8a764f761
appitionitsolution <applicationitsolution@gmail.com>
2022-08-23 00:33:22 +0300

    ts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/logo-test.png";
+import close from "../images/icons/close.svg";
 
 const Header = () => {
+  const [windowLog, setWindowLog] = useState(false);
+  const openWindowLog = () => {
+    setWindowLog(!windowLog);
+  };
   return (
     <header className="header">
       <div className="container">
@@ -42,6 +47,33 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+          <div className="header-sing">
+            <button className="header-sing-singin" onClick={openWindowLog}>
+              Вхід
+            </button>
+            <button className="header-sing-register">Реєстрація</button>
+          </div>
+        </div>
+        <div className={`window-singin ${windowLog ? "active" : ""}`}>
+          <div className="window-input">
+            <h2 className="window-title">Вхід до кабінету</h2>
+            <form className="window-form">
+              <input
+                className="window-form-email"
+                type="email"
+                placeholder="Ваша пошта"
+              />
+              <input
+                className="window-form-password"
+                type="password"
+                placeholder="Ваш пароль"
+              />
+              <button className="window-form-submit">Увійти</button>
+            </form>
+            <button className="window-close" onClick={openWindowLog}>
+              <img className="window-close-icon" src={close} alt="close" />
+            </button>
+          </div>
         </div>
       </div>
     </header>
